357c3481a29b0182eb189ec7e336e3b3451415fd
Xavier Alves Monteiro <75092505+xavier-monteiro@users.noreply.github.com>
2022-01-13 17:00:31 +0000

    User story: adicionar despesa. task 1,2,3 done

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports={
         connection:{
             host:'localhost',
             user:'postgres',
-            password:'1234', /* colocar a vossa pass */
+            password:'PASword!', /* colocar a vossa pass */
             database:'apishareabill',
         },
         debug:true,
--- src/routes/expenses.js
+++ src/routes/expenses.js
@@ -3,12 +3,33 @@ const express = require('express');
 module.exports = (app) => {
     const router = express.Router();
 
+    const validate = (req,res,next) => {
+        app.services.expense.validate({...req.body})
+        .then(()=>next())
+        .catch((err) => next(err));
+    };
+
     router.get('/', (req, res, next) => {
         app.services.expense.findAll()
         .then((result) => res.status(200).json(result))
         .catch((err) => next(err));
     });
 
-    return router
+    router.post('/',validate, (req, res, next) => {
+        console.log('**************************************');
+        console.log(req.body);
+        app.services.expense.save(req.body)
+        .then((result) => res.status(201).json(result[0]))
+        .catch((err) => next(err));
+    });
+
+    router.put('/:id',validate,(req,res,next) =>{
+        app.services.expense.update(req.params.id, req.body)
+            .then((result)=> res.status(200).json(result[0]))
+            .catch((err)=> next(err));
+    });
+    
+
+    return router;
 
 };

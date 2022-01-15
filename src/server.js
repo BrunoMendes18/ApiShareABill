6c79ed6c7662c9dcc314f43213f531add5cfd524
BrunoMendes18 <bjsm181818@gmail.com>
2022-01-15 22:24:03 +0000

    commit runners

--- src/server.js
+++ src/server.js
@@ -1,3 +1,13 @@
-const app = require('./app');
+const express = require('express');
 
-app.listen(3001);
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello World !!! Test #2');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);

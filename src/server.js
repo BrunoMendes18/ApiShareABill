743c99ee17a360a16dab23cf3546e91e3b3ea221
Vitor Silva <vitordinissilva@gmail.com>
2022-01-18 16:09:54 +0000

    Update src/server.js

--- src/server.js
+++ src/server.js
@@ -1,13 +1,3 @@
-const express = require('express');
+const app = require('./app')
 
-const PORT = 3000;
-const HOST = '0.0.0.0';
-
-const app = express();
-
-app.get('/', (req, res) => {
-  res.status(200).send('Hello World !!! Test #3');
-});
-
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.listen(3000);

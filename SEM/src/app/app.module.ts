97f46c3e7175c555bbd8385bdb2b1ffcb6a5e7d4
Xavier Alves Monteiro <75092505+xavier-monteiro@users.noreply.github.com>
2022-01-19 10:40:25 +0000

    updated login css

--- SEM/src/app/app.module.ts
+++ SEM/src/app/app.module.ts
@@ -13,7 +13,7 @@ import { NavBarComponent } from './components/nav-bar/nav-bar.component';
     AppComponent,
     LoginComponent,
     HomeComponent,
-    NavBarComponent
+    NavBarComponent,
   ],
   imports: [
     BrowserModule,
